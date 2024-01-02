520b977a5a15ae718722958c971d37c646b9b9e0
dogyeom <rlaehrua930@naver.com>
2024-01-02 11:22:46 +0900

    first commit

---
+++ src/test/java/bit38_7/MapConvertor/MapConvertorApplicationTests.java
@@ -0,0 +1,13 @@
+package bit38_7.MapConvertor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapConvertorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
