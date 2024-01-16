d7a4d1db5869aa399a7d3fa61da7b208428c6455
dogyeom <rlaehrua930@naver.com>
2024-01-16 14:10:43 +0900

    ㅁ

---
+++ src/main/java/bit38_7/MapConvertor/MapConvertorApplication.java
@@ -0,0 +1,13 @@
+package bit38_7.MapConvertor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapConvertorApplication {
+
+	public static void main(String[] args) {
+		 SpringApplication.run(MapConvertorApplication.class, args);
+	}
+
+}
